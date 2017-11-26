b76970a2ba5ffb39c12755e1b2baa61c29e282d2
david <david@gymnasiumleiden.nl>
2017-11-26 23:24:07 +0100

    init

---
+++ Blackbook/scripts/PF 373.zip - blackbook script/theme/install/default/template/completed.html.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * [PHPFOX_HEADER]
+ * 
+ * @copyright		[PHPFOX_COPYRIGHT]
+ * @author			Raymond Benc
+ * @package 		Phpfox
+ * @version 		$Id: completed.html.php 5350 2013-02-13 10:59:22Z Raymond_Benc $
+ */
+ 
+defined('PHPFOX') or exit('NO DICE!'); 
+
+?>
+<div class="completed_message">
+	{if $bIsUpgrade}
+	Successfully upgraded to phpFox version {$sUpgradeVersion}.
+	{else}
+	Successfully installed phpFox {$sUpgradeVersion}.
+	{/if}
+	<div class="p_4">
+			</div>
+</div>
+
+<a href="../index.php" class="installed_link">View Your Site</a>
+<br />
---
+++ README.md
@@ -0,0 +1,5 @@
+blackbook hack leak 23 nov 2017
+
+Leak for breached black book hidden service.
+
+more info in leak.txt
